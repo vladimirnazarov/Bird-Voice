c0fdb99e2c03470d1d6d2116ed6c99e40c510e95
Vladimir Nazarov <tooldiamond2014@gmail.com>
2023-05-13 18:22:16 +0300

    Main screen: inform page and it's resources has been done

--- app/src/main/java/by/ssrlab/birdvoice/main/fragments/record/InformPage.kt
+++ app/src/main/java/by/ssrlab/birdvoice/main/fragments/record/InformPage.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.ssrlab.birdvoice.databinding.FragmentMainInformPageBinding
 import by.ssrlab.birdvoice.main.fragments.BaseMainFragment
+import by.ssrlab.birdvoice.main.rv.InformAdapter
 
 class InformPage: BaseMainFragment() {
 
     private lateinit var binding: FragmentMainInformPageBinding
-//    private lateinit var adapter: InformPageAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,5 +22,13 @@ class InformPage: BaseMainFragment() {
         binding = FragmentMainInformPageBinding.inflate(layoutInflater)
 
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val rv = binding.informRv
+        rv.layoutManager = LinearLayoutManager(requireContext())
+        rv.adapter = InformAdapter()
     }
 }
